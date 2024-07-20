d2cbb2c26715b55ae611c718a8323a87dc2a30a7
Tom Baddeley <tbaddeley73@gmail.com>
2024-07-20 19:01:28 +0800

    push

---
+++ stock-market-simulator-be/stock-market-simulator-be/src/main/java/com/example/stockmarketsimulatorbe/StockMarketSimulatorBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketsimulatorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketSimulatorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketSimulatorBeApplication.class, args);
+	}
+
+}
---
+++ stock-market-simulator-be/stock-market-simulator-be/src/test/java/com/example/stockmarketsimulatorbe/StockMarketSimulatorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketsimulatorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketSimulatorBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
